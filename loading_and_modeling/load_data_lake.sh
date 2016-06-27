a5024937251869f4498e22570a146015df0a1ad6
sho-ohata <sho.ohata@gmail.com>
2016-06-26 21:00:25 -0400

    changes

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+mount -t ext4 /dev/xvdf /data
 ./start-hadoop.sh
 /data/start_postgres.sh
 
 su - w205
 cd /data
 
-wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
-
 mkdir files
+wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 
 unzip Nq* -d files
 
@@ -18,9 +18,9 @@ for f in *.csv; do
 	tail -n +2 "$f" > "${f}".tmp && mv "${f}".tmp "$f"
 done
 
-#for f in *.csv; do
-#	sed -i 's/\Not Available//g' "$f"
-#done
+for f in *.csv; do
+	sed -i 's/\Not Available//g' "$f"
+done
 
 mv "Hospital General Information.csv" hospitals.csv
 mv "Timely and Effective Care - Hospital.csv" effective_care.csv
@@ -30,10 +30,19 @@ mv "hvbp_hcahps_05_28_2015.csv" survey_responses.csv
 
 hdfs dfs -mkdir /user/w205/hospital_compare
 
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/Measures
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+
 hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
 hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
 hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
 hdfs dfs -put Measures.csv /user/w205/hospital_compare/Measures
 hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
+
+cd ..
+rm Nq*
 
 hdfs dfs -chmod -R 777 /user/w205/hospital_compare
--- setup_ucb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod a+rwx /data
-wget https://s3.amazonaws.com/ucbdatasciencew205/setup_ucb_complete_plus_postgres.sh
-chmod +x ./setup_ucb_complete_plus_postgres.sh
-./setup_ucb_complete_plus_postgres.sh /dev/xvdf
