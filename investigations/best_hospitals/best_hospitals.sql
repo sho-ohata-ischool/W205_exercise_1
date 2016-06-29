005c5dc1859a10a67bdf7d03d05813b386cf0f5b
sho-ohata <sho.ohata@gmail.com>
2016-06-28 20:40:40 -0400

    correcting mistakes

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -19,24 +19,24 @@ select h.hospital_name, sum(cast(score as int)) as avg_score
 			order by count_m desc
 			limit 5) top_measure
 			on ef.measure_id = top_measure.measure_id
-	group by h.hospital_name
+	group by h.hospital_name, h.provider_id
 	order by avg_score desc 
 	limit 10;
 
 --select hospitals where readmission rates for various procedures on average are better than the national rate
-select s1.hospital, hospital_better_count/hospital_total_count as hospital_ratio from (
-	select h.hospital, count(*) as hospital_total_count
+select s1.hospital_name, hospital_better_count/hospital_total_count as hospital_ratio from (
+	select h.hospital_name, count(*) as hospital_total_count
 		from readmissions r
 			inner join hospitals h
 				on r.provider_id = h.provider_id
-		group by h.hospital) s1 inner join (
-	select h.hospital, count(*) as hospital_better_count
+		group by h.hospital_name, h.provider_id) s1 inner join (
+	select h.hospital_name, count(*) as hospital_better_count
 		from readmissions r
 			inner join hospitals h
 				on r.provider_id = h.provider_id
 			where compared_to_national = 'Better than the National Rate'
-		group by h.hospital) s2 
-		on s1.hospital = s2.hospital
+		group by h.hospital_name, h.provider_id) s2 
+		on s1.hospital_name = s2.hospital_name
 	where hospital_total_count > 0 
 	and hospital_better_count > 0
 	order by hospital_ratio desc
